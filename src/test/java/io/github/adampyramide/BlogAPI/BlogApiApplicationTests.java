47432faa45dbe25e01776bfdd92008ab9e98c7f1
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-06-22 17:58:47 +0200

    Initial commit project

---
+++ src/test/java/io/github/adampyramide/BlogAPI/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.adam_elzahiri.BlogAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
