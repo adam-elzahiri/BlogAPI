3c2cb50e2686064278a9de8163d1e735cc963be6
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-06-23 03:45:49 +0200

    Added user registery feature

---
+++ src/main/java/io/github/adampyramide/BlogAPI/user/RegisterUserDTO.java
@@ -0,0 +1,6 @@
+package io.github.adam_elzahiri.BlogAPI.user;
+
+public record RegisterUserDTO (
+        String username,
+        String password
+) {}
---
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserRepository.java
@@ -0,0 +1,11 @@
+package io.github.adam_elzahiri.BlogAPI.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByUsername(String username);
+
+}
