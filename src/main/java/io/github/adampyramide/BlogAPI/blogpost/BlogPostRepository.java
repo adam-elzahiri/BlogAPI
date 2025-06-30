57e3f40ae75e6019b0dec160b58afde641ad97e0
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-06-30 23:57:47 +0200

    Remove unused imports
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostRepository.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostRepository.java
@@ -1,10 +1,7 @@
 package io.github.adam_elzahiri.BlogAPI.blogpost;
 
-import io.github.adam_elzahiri.BlogAPI.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Integer> {
