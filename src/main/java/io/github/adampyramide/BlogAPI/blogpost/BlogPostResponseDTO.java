2d9b35c823948628f2e82b1b226e5e5e09c282de
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-02 14:37:03 +0200

    Fix formatting
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostResponseDTO.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostResponseDTO.java
@@ -5,9 +5,11 @@ import io.github.adam_elzahiri.BlogAPI.user.PublicUserDTO;
 import java.time.LocalDateTime;
 
 public record BlogPostResponseDTO(
+
         int id,
         String title,
         String body,
         LocalDateTime createTime,
         PublicUserDTO author
+
 ) {}
--- src/main/java/io/github/adampyramide/BlogAPI/user/PublicUserDTO.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/PublicUserDTO.java
@@ -1,6 +1,8 @@
 package io.github.adam_elzahiri.BlogAPI.user;
 
 public record PublicUserDTO(
+
         int id,
         String username
+
 ) {}
