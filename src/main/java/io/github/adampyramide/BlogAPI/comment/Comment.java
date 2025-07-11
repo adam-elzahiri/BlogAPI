ad23d1b2410a0854a1cd695836daeff857c05480
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-11 11:40:26 +0200

    Set up comment entity fields
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/comment/Comment.java
+++ src/main/java/io/github/adampyramide/BlogAPI/comment/Comment.java
@@ -1,5 +1,27 @@
 package io.github.adam_elzahiri.BlogAPI.comment;
 
+import io.github.adam_elzahiri.BlogAPI.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
 
 }
