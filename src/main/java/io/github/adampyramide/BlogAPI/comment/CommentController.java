d156552c86947224d12a55f6ee6e85cd30bfdbf8
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-14 20:38:49 +0300

    Remove unused import
    Signed-off-by: Adam El Zahiri <adamxelz@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/comment/CommentController.java
+++ src/main/java/io/github/adampyramide/BlogAPI/comment/CommentController.java
@@ -1,7 +1,6 @@
 package io.github.adam_elzahiri.BlogAPI.comment;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
