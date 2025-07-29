240b6b25a5e7de569b7374539d6823dfc45be4c1
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-29 15:07:57 +0300

    refactor: Remove setter and use lombok setter to remove boilerplate
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/exception/CustomException.java
+++ src/main/java/io/github/adampyramide/BlogAPI/exception/CustomException.java
@@ -1,7 +1,9 @@
 package io.github.adam_elzahiri.BlogAPI.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class CustomException extends RuntimeException {
 
     private final HttpStatus httpStatus;
@@ -9,10 +11,6 @@ public class CustomException extends RuntimeException {
     public CustomException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
     }
 
 }
