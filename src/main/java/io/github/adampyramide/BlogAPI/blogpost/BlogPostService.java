e16a3d394e9574d4ccbbc97c091101942681cd9e
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-29 05:35:37 +0300

    Remove unused import
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/blogpost/BlogPostService.java
@@ -7,7 +7,6 @@ import io.github.adam_elzahiri.BlogAPI.security.SecurityUtils;
 import io.github.adam_elzahiri.BlogAPI.user.User;
 import io.github.adam_elzahiri.BlogAPI.user.UserService;
 import io.github.adam_elzahiri.BlogAPI.util.OwnershipValidator;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
