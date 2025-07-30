0e8fd9738d7d8814957f4eed7802baf227aa9011
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-07-30 23:27:16 +0300

    chore: update documentation to use 'Blog post' instead of 'blogpost'
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/UserService.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserService.java
@@ -1,7 +1,9 @@
 package io.github.adam_elzahiri.BlogAPI.user;
 
-import io.github.adam_elzahiri.BlogAPI.exception.CustomException;
+import io.github.adam_elzahiri.BlogAPI.error.ApiException;
 import io.github.adam_elzahiri.BlogAPI.filestorage.CloudinaryFileStorageService;
+import io.github.adam_elzahiri.BlogAPI.filestorage.FileValidationRule;
+import io.github.adam_elzahiri.BlogAPI.filestorage.MimeTypeRules;
 import io.github.adam_elzahiri.BlogAPI.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,8 @@ public class UserService {
 
     private final SecurityUtils securityUtils;
     private final CloudinaryFileStorageService fileStorageService;
+
+    private final FileValidationRule fileValidationRule = MimeTypeRules.IMAGE_ONLY;
 
     // ====================
     // Public methods
@@ -32,7 +36,11 @@ public class UserService {
         mapper.updateEntity(userRequest, user);
 
         if (userRequest.profilePicture() != null && !userRequest.profilePicture().isEmpty()) {
-            String profilePictureUrl = fileStorageService.save(userRequest.profilePicture(), "profile-pictures");
+            String profilePictureUrl = fileStorageService.save(
+                    userRequest.profilePicture(),
+                    "profile-pictures",
+                    fileValidationRule
+            );
             user.setProfilePictureUrl(profilePictureUrl);
         }
 
@@ -49,7 +57,11 @@ public class UserService {
 
     public User getUserOrThrow(Long id) {
         return repo.findById(id)
-                .orElseThrow(() -> new CustomException("User not found", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ApiException(
+                        HttpStatus.NOT_FOUND,
+                        "USER_NOT_FOUND",
+                        "User with ID " + id + " not found."
+                ));
     }
 
 }
