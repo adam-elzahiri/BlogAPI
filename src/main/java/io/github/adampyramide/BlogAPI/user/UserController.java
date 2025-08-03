00be84f4613f867d61020292bd86578b58757f06
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-08-04 01:23:36 +0300

    refactor: getUserById return UserProfileResponse instead of UserPreviewResponse
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/UserController.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserController.java
@@ -2,6 +2,7 @@ package io.github.adam_elzahiri.BlogAPI.user;
 
 import io.github.adam_elzahiri.BlogAPI.user.dto.UserPreviewResponse;
 import io.github.adam_elzahiri.BlogAPI.user.dto.UpdateUserRequest;
+import io.github.adam_elzahiri.BlogAPI.user.dto.UserProfileResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -55,7 +56,7 @@ public class UserController {
             }
     )
     @GetMapping("/{id}")
-    public ResponseEntity<UserPreviewResponse> getUserById(@PathVariable Long id) {
+    public ResponseEntity<UserProfileResponse> getUserById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getUserById(id));
     }
 
