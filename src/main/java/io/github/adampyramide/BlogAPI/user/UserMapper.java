f6c6d9e0843c63a684c1e056596c4af1438b2df0
Adam El Zahiri <adamelzahiri.dev@gmail.com>
2025-08-04 01:20:18 +0300

    refactor: add afterMapping for userResponses to handle enrichment
    Signed-off-by: Adam El Zahiri <adamelzahiri.dev@gmail.com>

--- src/main/java/io/github/adampyramide/BlogAPI/user/UserMapper.java
+++ src/main/java/io/github/adampyramide/BlogAPI/user/UserMapper.java
@@ -1,26 +1,53 @@
 package io.github.adam_elzahiri.BlogAPI.user;
 
 import io.github.adam_elzahiri.BlogAPI.auth.AuthRequest;
+import io.github.adam_elzahiri.BlogAPI.filestorage.CloudinaryFileStorageService;
+import io.github.adam_elzahiri.BlogAPI.filestorage.FileValidationRule;
+import io.github.adam_elzahiri.BlogAPI.filestorage.MimeTypeRules;
 import io.github.adam_elzahiri.BlogAPI.user.dto.UserPreviewResponse;
 import io.github.adam_elzahiri.BlogAPI.user.dto.UpdateUserRequest;
 import io.github.adam_elzahiri.BlogAPI.user.dto.UserProfileResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(
         componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
 )
-public interface UserMapper {
+public abstract class UserMapper {
 
+    // DEPENDENCIES
+    @Autowired
+    private CloudinaryFileStorageService fileStorageService;
+
+    // VARIABLES
+    private static final FileValidationRule fileValidationRule = MimeTypeRules.IMAGE_ONLY;
+
+    // CONVERTERS
     @Mapping(target = "id", ignore = true)
-    User authDTOToEntity(AuthRequest dto);
+    public abstract User toEntity(AuthRequest dto);
 
-    UserPreviewResponse toUserPreviewResponse(User user);
+    public abstract UserPreviewResponse toUserPreviewResponse(User user);
 
+    public abstract UserProfileResponse toUserProfileResponse(User user);
+
+    // UPDATERS
     @Mapping(target = "id", ignore = true)
-    void updateEntity(UpdateUserRequest request, @MappingTarget User entity);
+    public abstract void updateEntity(UpdateUserRequest request, @MappingTarget User entity);
+
+    // ENRICHERS
+    @AfterMapping
+    protected void enrichUserPreviewResponse(User user, @MappingTarget UserPreviewResponse response) {
+        response.setAvatarUrl(getAvatarUrl(user));
+    }
+
+    @AfterMapping
+    protected void enrichUserProfileResponse(User user, @MappingTarget UserProfileResponse response) {
+        response.setAvatarUrl(getAvatarUrl(user));
+    }
+
+    private String getAvatarUrl(User user) {
+        return fileStorageService.getUrl(user.getAvatarId(), fileValidationRule);
+    }
 
 }
